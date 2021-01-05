78b4f8dfa1b9cc6c01633bacd2515becda4d7521
George Vlahavas <vlahavas@gmail.com>
2021-01-05 03:21:29 +0200

    Rename variables with restricted names

--- gtkkeyboardsetup/gtkkeyboardsetup.py
+++ gtkkeyboardsetup/gtkkeyboardsetup.py
@@ -142,12 +142,12 @@ def availablekeymaps():
                 break
             if not line.startswith('#'):
                 registered.append(line.partition('|')[0])
-    for type in availablekeybtypes():
-        for filename in os.listdir(keymapdir + type):
+    for keyboard_type in availablekeybtypes():
+        for filename in os.listdir(keymapdir + keyboard_type):
             if filename.endswith('.map.gz'):
-                map = filename.replace('.map.gz', '')
-                if map in registered:
-                    keymaps.append([type, map])
+                key_map = filename.replace('.map.gz', '')
+                if key_map in registered:
+                    keymaps.append([keyboard_type, key_map])
                 # else:
                 # print "WARNING: {0} keymap is not specified in keymaps
                 # list.".format(map)
